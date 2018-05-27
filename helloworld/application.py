9e568427d73dfc5eb3a9048ebb84c329cb03a4f8
EC2 Default User <ec2-user@ip-172-31-40-150.us-east-2.compute.internal>
2018-05-27 15:44:53 +0000

    added post to dynamo

--- helloworld/application.py
+++ helloworld/application.py
@@ -3,8 +3,10 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0])))
 import json
 import requests
+import boto3
+import datetime
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 from helloworld.flaskrun import flaskrun
 
 application = Flask(__name__)
@@ -25,6 +27,44 @@ def get_ip_meta():
     user_ip = str(request.environ['REMOTE_ADDR'])
     service_url = 'http://ipinfo.io/{}'.format(user_ip) 
     return requests.get(service_url).json()
+   
+@application.route('/temp/<temp>', methods=['POST'])
+def get_temp(temp):
+    # get ip metadata from the fuction
+    response = get_ip_meta()
+    # create a session for boto to access the credentials that the ec2 holds
+    my_ses = boto3.Session(region_name = 'us-east-2')
+    # connect to the resource dynmodb using the session
+    dynamodb = my_ses.resource('dynamodb')
+    # refer to the table
+    table = dynamodb.Table('eb_try_logger')
+
+    item={
+    'ip_address': str(response), 
+    'path': temp,
+    'datetime': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    'time': datetime.datetime.now().strftime("%H:%M:%S"),
+    'ip_meta' : response, # res_data
+    'name':'chilkibilki'
+    }
     
+    print(item)
+    # insert the item
+    table.put_item(Item=item)
+    
+    return Response(json.dumps(item), mimetype='application/json', status=200)
+
+
+@application.route('/bi', methods=['GET'])
+def get_bi():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('eb_try_logger')
+    resp = table.scan()
+
+    #return Response(json.dumps(str(resp)), mimetype='application/json', status=200)
+    return render_template('index.html', response=str(resp), title='bi')
+
+    
+
 if __name__ == '__main__':
     flaskrun(application)
